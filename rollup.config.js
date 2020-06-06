0f2fa9d4c4d94917c1eea514f6b84ffd564bf76c
Adam Sawicki <adamsawicki89@gmail.com>
2020-06-06 13:15:17 +0200

    Make the package compatibile with commonjs modules

--- rollup.config.js
+++ rollup.config.js
@@ -1,33 +1,32 @@
-import nodeResolve from 'rollup-plugin-node-resolve';
-import babel       from 'rollup-plugin-babel';
-import replace     from 'rollup-plugin-replace';
-import commonjs    from 'rollup-plugin-commonjs';
-import uglify      from 'rollup-plugin-uglify';
+import nodeResolve from "rollup-plugin-node-resolve";
+import babel from "rollup-plugin-babel";
+import replace from "rollup-plugin-replace";
+import commonjs from "rollup-plugin-commonjs";
+import uglify from "rollup-plugin-uglify";
 
 const env = process.env.NODE_ENV;
 
 const config = {
-  input: 'src/index.js',
-  external: ['react', 'react-dom'],
+  input: "src/index.js",
   output: {
-    format: 'umd',
-    name: 'npmPackageES6Boilerplate',
+    format: "cjs",
+    name: "googleDriveUpload",
   },
 
   plugins: [
     nodeResolve(),
     babel({
-      exclude: '**/node_modules/**',
+      exclude: "**/node_modules/**",
       runtimeHelpers: true,
     }),
     replace({
-      'process.env.NODE_ENV': JSON.stringify(env),
+      "process.env.NODE_ENV": JSON.stringify(env),
     }),
     commonjs(),
   ],
 };
 
-if (env === 'production') {
+if (env === "production") {
   config.plugins.push(
     uglify({
       compress: {
@@ -36,7 +35,7 @@ if (env === 'production') {
         unsafe_comps: true,
         warnings: false,
       },
-    }),
+    })
   );
 }
 
